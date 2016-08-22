c729d30be3f55bbd136637db646be428a4f631ca
Nicolas <nicolas@kwiatkowski.fr>
2016-08-22 17:04:18 +0200

    début de la factorisation à l'aide d'objets

--- WebContent/js/snakemulti.js
+++ WebContent/js/snakemulti.js
@@ -1,56 +1,42 @@
-//alert('Welcome to the best snake game in the world !');
+//Code by Anorgar, rules by Emi ly
 
+//c'est une nouvelle version du projet, en utilisant les objet js pour rendre le code beaucoup plus propre
+
+//Variables a modifier pour changer les paramètres du snake
 //Hauteur de la grille
-var hauteur = 30;
-//Largeure de la grille
-var largeur = 30;
-//vitesse maximum
-var vitesseMax=50;
-//vitesse initiale + 1 (car lors de la création de la première pomme, on perd 1 en vitesse)
-var vitesse=101;
-
-//Création des variables globales
-var directionNoir = "droite";
-var directionRouge = "droite";
-var positionSnakeNoir = new Array();
-var positionSnakeRouge = new Array();
-var indexPomme;
-var indexTemp;
-var indexTete;
-var directionBoucleNoir;
-var directionBoucleRouge;
-var scoreNoir=0;
-var scoreRouge=0;
-var flagN=false; //true si le snake noir meurt
-var flagR=false; //true si le snake rouge meurt
-
-//création du snake
-positionSnakeNoir = [ "1 1", "1 2", "1 3" ];
-positionSnakeRouge = [ "10 1", "10 2", "10 3" ];
-
-//mise en page du score
-document.getElementById("scorenoir").style.width = "100px";
-document.getElementById("scorenoir").style.height = "20px";
-document.getElementById("scorenoir").style.border = "thick solid black";
-
-document.getElementById("scorerouge").style.width = "100px";
-document.getElementById("scorerouge").style.height = "20px";
-document.getElementById("scorerouge").style.border = "thick solid red";
-
-//initialisation des scores
-document.getElementById("scorenoir").innerHTML=scoreNoir;
-document.getElementById("scorerouge").innerHTML=scoreRouge;
+var hauteur = 20;
+// Largeure de la grille
+var largeur = 20;
+// vitesse maximum
+var vitesseMax = 50;
+// vitesse initiale + 1 (car lors de la création de la première pomme, on perd 1
+// en vitesse)
+var vitesseRouge = 100; // vitesse du snake rouge initiale
+var vitesseNoir = 100; // vitesse du snake noir initiale
+// nombre de pommes a faire apparaitre à la mort du premier snake
+var nbrPomme = 19;
+var nbrTour = 10000; // durée du bonus final en nombre de millisecondes
+var nbrTourBonus = 10000; // durée des bonus classiques en nombre de
+							// millisecondes
+var nbrTourVraiBonus = 10000; // durée des vrais bonus classiques en nombre de
+								// millisecondes
+var probaJaune = 10;// Probabilité d'apparition d'une pomme jaune
+var probaBleue = 10; // Probabilité d'apparition d'une pomme bleue
+var probaMaron = 10; // Probabilité d'apparition d'une pomme maron
+var probaViolet = 10; // Probabilité d'apparition d'une pomme maron
 
 // Création du tableau pour le snake
-var table = document.createElement("table");
+var table = document.getElementById(("table"));
 table.style.backgroundColor = "#87CEFA";
-for (var i = 0; i <= largeur+1; i++) {
+table.cellSpacing = "0px";
+// table.cellSpacing = "0px";
+for (var i = 0; i <= largeur + 1; i++) {
 	var row = document.createElement("tr");
-	for (var j = 0; j <= hauteur+1; j++) {
+	for (var j = 0; j <= hauteur + 1; j++) {
 		var cell = document.createElement("td");
 		cell.appendChild(document.createTextNode(""));
-		cell.style.width = "10px";
-		cell.style.height = "10px";
+		cell.style.width = "8px";
+		cell.style.height = "8px";
 		cell.setAttribute("id", i + " " + j);
 		row.appendChild(cell);
 	}
@@ -58,267 +44,138 @@ for (var i = 0; i <= largeur+1; i++) {
 }
 document.getElementById("debut").appendChild(table);
 
-document.getElementById(positionSnakeNoir[0]).style.backgroundColor = "black";
-document.getElementById(positionSnakeNoir[1]).style.backgroundColor = "black";
-document.getElementById(positionSnakeNoir[2]).style.backgroundColor = "black";
+submit.onclick = function newGame(){
 
-document.getElementById(positionSnakeRouge[0]).style.backgroundColor = "red";
-document.getElementById(positionSnakeRouge[1]).style.backgroundColor = "red";
-document.getElementById(positionSnakeRouge[2]).style.backgroundColor = "red";
-
-//faire apparaitre une pomme:
-apparaitrePomme();
-
-//déplacement du snake noir
-window.addEventListener("keydown", function(event) {
-	switch (event.keyCode) {
-	case 37:
-		if (directionBoucleNoir != "droite") {
-			directionNoir = "gauche";
-		}
-		break;
-	case 38:
-		if (directionBoucleNoir != "bas") {
-			directionNoir = "haut";
-		}
-		break;
-	case 39:
-		if (directionBoucleNoir != "gauche") {
-			directionNoir = "droite";
-		}
-		break;
-	case 40:
-		if (directionBoucleNoir != "haut") {
-			directionNoir = "bas";
-		}
-		break;
-	case 81:
-		if (directionBoucleRouge != "droite") {
-			directionRouge = "gauche";
-		}
-		break;
-	case 90:
-		if (directionBoucleRouge != "bas") {
-			directionRouge = "haut";
-		}
-		break;
-	case 68:
-		if (directionBoucleRouge != "gauche") {
-			directionRouge = "droite";
-		}
-		break;
-	case 83:
-		if (directionBoucleRouge != "haut") {
-			directionRouge = "bas";
-		}
-		break;
-	}
-});
-
-var boucle = setInterval(deplacementSnake, vitesse);
-
-function deplacementSnake() {
-	//on déplace d'abord le noir, puis le rouge (pour pas de racisme)
-	//si le noir est vivant
-	if (flagN==false){
-		indexTemp = 0;
-		switch (directionNoir) {
-		case "droite":
-			indexTemp = positionSnakeNoir[positionSnakeNoir.length - 1].split(' ')[1];
-			indexTemp = parseInt(indexTemp) + 1;
-			if (parseInt(indexTemp) > largeur) {
-				indexTemp = 0;
-			}
-			indexTete = positionSnakeNoir[positionSnakeNoir.length - 1].split(' ')[0] + " "
-					+ indexTemp;
-			directionBoucleNoir = "droite";
-			break;
-		case "gauche":
-			indexTemp = positionSnakeNoir[positionSnakeNoir.length - 1].split(' ')[1];
-			indexTemp = parseInt(indexTemp) - 1;
-			if (parseInt(indexTemp) < 0) {
-				indexTemp = largeur;
-			}
-			indexTete = positionSnakeNoir[positionSnakeNoir.length - 1].split(' ')[0] + " "
-					+ indexTemp;
-			directionBoucleNoir = "gauche";
-			break;
-		case "haut":
-			indexTemp = positionSnakeNoir[positionSnakeNoir.length - 1].split(' ')[0];
-			indexTemp = parseInt(indexTemp) - 1;
-			if (parseInt(indexTemp) < 0) {
-				indexTemp = hauteur;
-			}
-			indexTete = indexTemp + " "
-					+ positionSnakeNoir[positionSnakeNoir.length - 1].split(' ')[1];
-			directionBoucleNoir = "haut";
-			break;
-		case "bas":
-			indexTemp = positionSnakeNoir[positionSnakeNoir.length - 1].split(' ')[0];
-			indexTemp = parseInt(indexTemp) + 1;
-			if (parseInt(indexTemp) > hauteur) {
-				indexTemp = 0;
-			}
-			indexTete = indexTemp + " "
-					+ positionSnakeNoir[positionSnakeNoir.length - 1].split(' ')[1];
-			directionBoucleNoir = "bas";
-			break;
-		}
-		
-		//Si on est sur une pomme:
-		document.getElementById(positionSnakeNoir[0]).style.backgroundColor = "#87CEFA";
-		for (var i=0; i<positionSnakeNoir.length-1;i++){
-			positionSnakeNoir[i] = positionSnakeNoir[i+1];
-		}
-		positionSnakeNoir[positionSnakeNoir.length-1] = indexTete;
-		
-		//comme le rouge mange le noir, il faut le réafficher
-		for (var i=0; i<positionSnakeNoir.length;i++){
-			document.getElementById(positionSnakeNoir[i]).style.backgroundColor = "black";
-		}
-		
-		//Si le snake se mord la queue
-		for (var i=0; i<positionSnakeNoir.length-1;i++){
-			if(positionSnakeNoir[i] == indexTete){
-				flagN=true;
-				for (var j=0; j<positionSnakeNoir.length;j++){
-					document.getElementById(positionSnakeNoir[j]).style.backgroundColor = "#87CEFA";
-				}
-				if (flagR){
-					clearInterval(boucle);
-					alert("Vous avez perdu :'(");
-					break;
-				}
-			}
-		}
-		if (indexTete == indexPomme){
-			//on fait apparaitre une nouvelle pomme
-			apparaitrePomme();
-			//on fait grandir le snake
-			positionSnakeNoir.push(indexTete);
-			//ajout du nouveau score
-			scoreNoir = scoreNoir + 10;
-			document.getElementById("scorenoir").innerHTML=scoreNoir;
-			clearInterval(boucle);
-			boucle = setInterval(deplacementSnake, vitesse);
-		}
-	}
+	// création des objet snakes
+	var snakeNoir = {
+//		boucle: new setInterval(),
+		positionSnake : [ "1 1", "1 2", "1 3" ], //tableau contenant les indexes des cases du snake
+		directionSnake : [ "droite", "droite", "droite" ], //tableau contenant les directions des cases du snake
+		miam : [ false, false, false ], //tableau contenant l'estomac du snake
+		snakeBody : [ "url('img/queueDroiteNoir.png')",
+				"url('img/queueGaucheNoir.png')", "url('img/queueHautNoir.png')",
+				"url('img/queueBasNoir.png')", "url('img/teteNoirDroite.png')",
+				"url('img/teteNoirGauche.png')", "url('img/teteNoirHaute.png')",
+				"url('img/teteNoirBas.png')", "url('img/corpDroiteNoir.png')",
+				"url('img/corpHautNoir.png')", "url('img/corpHautDroiteNoir.png')",
+				"url('img/corpGaucheHautNoir.png')",
+				"url('img/corpBasDroiteNoir.png')",
+				"url('img/corpBasGaucheNoir.png')", "url('img/miamNoir.png')" ], // tableau contenant les images permettant de dessiner le snake
+		score : 0, //score du snake
+		victoire: 0, //nombre de victoire du snake
+		vitesse: vitesseNoir, //vitesse du snake
+		nomScore: "scorenoir", //id de la case dans laquelle on affiche le score noir
+		direction: "droite", // direction du snake dans une boucle de jeu
+		mort: false, //permet de savoir si le snake est vivant
+		tourFinal: 0, //permet de savoir conbient de temps va durer le bonus final
+		flagBonus: false, //permet de savoir si le snake est sous l'effet d'un bonus
+		tourBonus: 0, //permet de savoir conbien de temps va durer le bonus
+		flagMalus: false, //permet de savoir si on est sous l'effet d'un malus
+		tourMalus: 0 //permet de savoir combien de temps va durer le malus
+	};
 	
+	var snakeRouge = {
+//		boucle: new setInterval(),
+		positionSnake : [ "10 1", "10 2", "10 3" ], //tableau contenant les indexes des cases du snake
+		directionSnake : [ "droite", "droite", "droite" ], //tableau contenant les directions des cases du snake
+		miam : [ false, false, false ], //tableau contenant l'estomac du snake
+		snakeBody : ["url('img/queueDroiteRouge.png')","url('img/queueGaucheRouge.png')","url('img/queueHautRouge.png')","url('img/queueBasRouge.png')",
+                     "url('img/teteRougeDroite.png')","url('img/teteRougeGauche.png')","url('img/teteRougeHaute.png')","url('img/teteRougeBas.png')",
+                     "url('img/corpDroiteRouge.png')","url('img/corpHautRouge.png')",
+                     "url('img/corpHautDroiteRouge.png')","url('img/corpGaucheHautRouge.png')","url('img/corpBasDroiteRouge.png')","url('img/corpBasGaucheRouge.png')",
+                     "url('img/miamRouge.png')"], // tableau contenant les images permettant de dessiner le snake
+		score : 0, //score du snake
+		victoire: 0, //nombre de victoire du snake
+		nomScore: "scorerouge", //id de la case dans laquelle on affiche le score rouge
+		vitesse: vitesseRouge, //vitesse du snake
+		direction: "droite", // direction du snake dans une boucle de jeu
+		mort: false, //permet de savoir si le snake est vivant
+		tourFinal: 0, //permet de savoir conbient de temps va durer le bonus final
+		flagBonus: false, //permet de savoir si le snake est sous l'effet d'un bonus
+		tourBonus: 0, //permet de savoir conbien de temps va durer le bonus
+		flagMalus: false, //permet de savoir si on est sous l'effet d'un malus
+		tourMalus: 0 //permet de savoir combien de temps va durer le malus
+	};
 	
-	//on déplace d'abord le noir, puis le rouge (pour pas de racisme)
-	//si le snake est vivant
-	if (flagR==false){
-		indexTemp = 0;
-		switch (directionRouge) {
-		case "droite":
-			indexTemp = positionSnakeRouge[positionSnakeRouge.length - 1].split(' ')[1];
-			indexTemp = parseInt(indexTemp) + 1;
-			if (parseInt(indexTemp) > largeur) {
-				indexTemp = 0;
-			}
-			indexTete = positionSnakeRouge[positionSnakeRouge.length - 1].split(' ')[0] + " "
-					+ indexTemp;
-			directionBoucleRouge = "droite";
-			break;
-		case "gauche":
-			indexTemp = positionSnakeRouge[positionSnakeRouge.length - 1].split(' ')[1];
-			indexTemp = parseInt(indexTemp) - 1;
-			if (parseInt(indexTemp) < 0) {
-				indexTemp = largeur;
-			}
-			indexTete = positionSnakeRouge[positionSnakeRouge.length - 1].split(' ')[0] + " "
-					+ indexTemp;
-			directionBoucleRouge = "gauche";
-			break;
-		case "haut":
-			indexTemp = positionSnakeRouge[positionSnakeRouge.length - 1].split(' ')[0];
-			indexTemp = parseInt(indexTemp) - 1;
-			if (parseInt(indexTemp) < 0) {
-				indexTemp = hauteur;
-			}
-			indexTete = indexTemp + " "
-					+ positionSnakeRouge[positionSnakeRouge.length - 1].split(' ')[1];
-			directionBoucleRouge = "haut";
-			break;
-		case "bas":
-			indexTemp = positionSnakeRouge[positionSnakeRouge.length - 1].split(' ')[0];
-			indexTemp = parseInt(indexTemp) + 1;
-			if (parseInt(indexTemp) > hauteur) {
-				indexTemp = 0;
-			}
-			indexTete = indexTemp + " "
-					+ positionSnakeRouge[positionSnakeRouge.length - 1].split(' ')[1];
-			directionBoucleRouge = "bas";
-			break;
-		}
-		
-		//Si on est sur une pomme:
-		document.getElementById(positionSnakeRouge[0]).style.backgroundColor = "#87CEFA";
-		for (var i=0; i<positionSnakeRouge.length-1;i++){
-			positionSnakeRouge[i] = positionSnakeRouge[i+1];
-		}
-		positionSnakeRouge[positionSnakeRouge.length-1] = indexTete;
-		
-		for (var i=0; i<positionSnakeRouge.length;i++){
-			document.getElementById(positionSnakeRouge[i]).style.backgroundColor = "red";
-		}
-		
-		//Si le snake se mord la queue
-		for (var i=0; i<positionSnakeRouge.length-1;i++){
-			if(positionSnakeRouge[i] == indexTete){
-				flagR=true;
-				for (var j=0; j<positionSnakeRouge.length;j++){
-					document.getElementById(positionSnakeRouge[j]).style.backgroundColor = "#87CEFA";
-				}
-				if (flagN){
-					clearInterval(boucle);
-					alert("Vous avez perdu :'(");
-					break;
-				}
-			}
-		}
-		if (indexTete == indexPomme){
-			//on fait apparaitre une nouvelle pomme
-			apparaitrePomme();
-			//on fait grandir le snake
-			positionSnakeRouge.push(indexTete);
-			//ajout du nouveau score
-			scoreRouge = scoreRouge + 10;
-			document.getElementById("scorerouge").innerHTML=scoreRouge;
-			clearInterval(boucle);
-			boucle = setInterval(deplacementSnake, vitesse);
-		}
-	}
+	//création des pommes
+	var pommes={
+			
+	};
 	
+	//on affiche les scores
+	afficherScore(snakeNoir);
+	afficherScore(snakeRouge);
+	//on affiche les snakes
+	afficherSnake(snakeNoir);
+	afficherSnake(snakeRouge);
+
 }
 
-function apparaitrePomme() {
-	//augmentation progressive de la vitesse
-	vitesse = vitesse -1;
-	if (vitesse < vitesseMax){
-		vitesse = vitesseMax;
+
+
+
+function afficherSnake(snake){		
+	//on affiche la queue avec deux cas possible
+	//si on a mangé une pomme à cet index
+	if (snake.miam[0]){
+		document.getElementById(snake.positionSnake[0]).style.backgroundImage = snake.snakeBody[14];
+	} //il faut prendre en compte la rotation de la queue
+	else if(snake.directionSnake[0]!=snake.directionSnake[1]){ 
+		switch(snake.directionSnake[1]){
+		case "droite": document.getElementById(snake.positionSnake[0]).style.backgroundImage = snake.snakeBody[0]; break;
+		case "gauche": document.getElementById(snake.positionSnake[0]).style.backgroundImage = snake.snakeBody[1]; break;
+		case "haut": document.getElementById(snake.positionSnake[0]).style.backgroundImage = snake.snakeBody[2]; break;
+		case "bas": document.getElementById(snake.positionSnake[0]).style.backgroundImage = snake.snakeBody[3]; break;
+		}
 	}
-	var ligne;
-	var colonne;
-	var flag = false;
-	do {
-		flag = false;
-		ligne = Math.floor(Math.random() * largeur);
-		colonne = Math.floor(Math.random() * hauteur);
-		indexPomme= colonne + " " + ligne;
-		for (var i = 0; i<positionSnakeNoir.length; i++){
-			if (indexPomme == positionSnakeNoir[i]){
-				flag = true;
+	
+	else { //sinon, il faut afficher la queue dans le bon sens
+		switch(snake.directionSnake[0]){
+		case "droite": document.getElementById(snake.positionSnake[0]).style.backgroundImage = snake.snakeBody[0]; break;
+		case "gauche": document.getElementById(snake.positionSnake[0]).style.backgroundImage = snake.snakeBody[1]; break;
+		case "haut": document.getElementById(snake.positionSnake[0]).style.backgroundImage = snake.snakeBody[2]; break;
+		case "bas": document.getElementById(snake.positionSnake[0]).style.backgroundImage = snake.snakeBody[3]; break;
+		}
+	}
+	//Le reste du corps, c'est la merde...
+	for (var i=1; i<snake.positionSnake.length-1;i++){
+		//premier cas simple : on est en train de manger une pomme à cet index
+		if (snake.miam[i]){
+			document.getElementById(snake.positionSnake[i]).style.backgroundImage = snake.snakeBody[14];
+		} else //sinon, on regarde s'il n'a a pas de changement de direction
+		if (snake.directionSnake[i+1]==snake.directionSnake[i]){ 
+			switch(snake.directionSnake[i]){
+			case "droite":
+			case "gauche": document.getElementById(snake.positionSnake[i]).style.backgroundImage = snake.snakeBody[8]; break;
+			case "haut":
+			case "bas": document.getElementById(snake.positionSnake[i]).style.backgroundImage = snake.snakeBody[9]; break;
 			}
 		}
-		for (var i = 0; i<positionSnakeRouge.length; i++){
-			if (indexPomme == positionSnakeRouge[i]){
-				flag = true;
+		//sinon, il faut faire un coin
+		else {
+			//ça va être 4 if moche, mais ça va marcher
+			if ((snake.directionSnake[i+1]=="haut")&&(snake.directionSnake[i]=="droite")||(snake.directionSnake[i+1]=="gauche")&&(snake.directionSnake[i]=="bas")){
+				document.getElementById(snake.positionSnake[i]).style.backgroundImage = snake.snakeBody[11];
+			} else
+			if ((snake.directionSnake[i+1]=="haut")&&(snake.directionSnake[i]=="gauche")||(snake.directionSnake[i+1]=="droite")&&(snake.directionSnake[i]=="bas")){
+				document.getElementById(snake.positionSnake[i]).style.backgroundImage = snake.snakeBody[10];
+			}else
+			if ((snake.directionSnake[i+1]=="bas")&&(snake.directionSnake[i]=="gauche")||(snake.directionSnake[i+1]=="droite")&&(snake.directionSnake[i]=="haut")){
+				document.getElementById(snake.positionSnake[i]).style.backgroundImage = snake.snakeBody[12];
+			} else {
+				document.getElementById(snake.positionSnake[i]).style.backgroundImage = snake.snakeBody[13];
 			}
 		}
-	} while (flag);
-	
-	
-	document.getElementById(indexPomme).style.backgroundColor = "green";
-	
+	}
+	//on affiche la tête
+	switch(snake.directionSnake[snake.directionSnake.length-1]){
+	case "droite": document.getElementById(snake.positionSnake[snake.positionSnake.length-1]).style.backgroundImage = snake.snakeBody[4]; break;
+	case "gauche": document.getElementById(snake.positionSnake[snake.positionSnake.length-1]).style.backgroundImage = snake.snakeBody[5]; break;
+	case "haut": document.getElementById(snake.positionSnake[snake.positionSnake.length-1]).style.backgroundImage = snake.snakeBody[6]; break;
+	case "bas": document.getElementById(snake.positionSnake[snake.positionSnake.length-1]).style.backgroundImage = snake.snakeBody[7]; break;
+	}
+}
+
+function afficherScore(snake){
+	document.getElementById(snake.nomScore).innerHTML=snake.score;
 }
